9c478c9c967b1d23a1edcbbf43344f6d272c0eba
caiquemaverik2 <caiquemaverik2@gmail.com>
2022-11-29 09:37:07 -0300

    Conclusão do Curso

---
+++ src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -0,0 +1,7 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+    {nome: 'Liderança em Design', src: livro, id: 1},
+    {nome: 'Cangaceiro em JavaScript: Uma aventura no sertao da programação', src:livro, id: 3},
+    {nome: 'Apache Kafta e Spring Boot', src:livro, id: 4},
+]
